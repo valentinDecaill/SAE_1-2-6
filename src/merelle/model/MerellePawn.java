8d31cb892603c4626f82fddc21ee1645e58606a0
valou <valentin.decailloz@edu.umlp.fr>
2026-05-04 17:09:47 +0200

    MerellePawn.java

--- src/merelle/model/MerellePawn.java
+++ src/merelle/model/MerellePawn.java
@@ -1,4 +1,26 @@
 package merelle.model;
 
-public class MerellePawn {
+import boardifier.model.ElementTypes;
+import boardifier.model.GameElement;
+import boardifier.model.GameStageModel;
+
+/**
+ * A basic pawn element, with only 2 fixed parameters : number and color
+ * There are no setters because the state of a Hole pawn is fixed.
+ */
+public class MerellePawn extends GameElement {
+
+    private int color;
+    public static int PAWN_BLACK = 0;
+    public static int PAWN_RED = 1;
+
+    public MerellePawn(int color, GameStageModel gameStageModel) {
+        super(gameStageModel);
+        ElementTypes.register("merellepawn", 50);
+        this.type = ElementTypes.getType("merellepawn");
+        this.color = color;
+    }
+    public int getColor() {
+        return color;
+    }
 }
